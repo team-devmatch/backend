319e0eaac6f58b4a5c3dc1fea30801ef11f0947a
leejaewook <l53941542@gmail.com>
2026-04-22 11:40:41 +0900

    로그인 수정

---
+++ src/main/java/com/team03/project1/domain/festival/entity/FestivalEntity.java
@@ -0,0 +1,4 @@
+package com.team03.project1.domain.festival.entity;
+
+public class FestivalEntity {
+}
---
+++ src/main/java/com/team03/project1/domain/user/dto/UpdateImageDto.java
@@ -0,0 +1,4 @@
+package com.team03.project1.domain.user.dto;
+
+public class UpdataImageDto {
+}
